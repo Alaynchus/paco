af3a62a89c78a7222771f02e60fc3b333bd7ddf4
alayn <alaynmarcos@gmail.com>
2021-05-21 10:43:45 +0200

    fsdsdfs

---
+++ Prubajdoc/src/cine/Cine.java
@@ -0,0 +1,58 @@
+package cine;
+
+import java.util.ArrayList;
+
+public class Cine {
+
+	private String nombre;
+	private ArrayList<Pelicula> listaPeliculas;
+	private Pelicula[] salasocupadas;
+
+	public Cine(String nombre, int numSalas) {
+		
+	}
+
+	public int salaPeli(String nombrepeli, int duracion, int anio) {
+		return -1;
+	}
+
+	public void anadePeli(Pelicula pelicula) {
+
+	}
+
+	public void anadePeli(Pelicula pelicula, int numSala) {
+
+	}
+
+	public void ver() {
+
+	}
+
+	public void quitaPeli(String nombre) {
+
+	}
+
+	public int[] libres() {
+		return null;
+	}
+
+	public void cambiaDeSala(Pelicula pelicula) {
+
+	}
+
+	public static boolean yaEsta(int[] array, int sala) {
+		return false;
+	}
+
+	public void reseteaCine() {
+
+	}
+
+	public ArrayList<Pelicula> pelisMasCortas(int duracion) {
+		return null;
+	}
+
+	public ArrayList<Pelicula> pelisMasCortas(int horas, int minutos) {
+		return null;
+	}
+}
---
+++ Prubajdoc/src/cine/Pelicula.java
@@ -0,0 +1,67 @@
+package cine;
+/**
+ * 
+ * @author escay
+ *
+ */
+public class Pelicula {
+	/**
+	 * juan
+	 */
+	private String nombre;
+	/**
+	 * Atributo duracion que devuelve un int
+	 */
+	public int duracion;
+	/**
+	 * Atributo duracion que devuelve un int
+	 */
+	public int anio;
+	/**
+	 * 
+	 * @param nombre tipo string
+	 * @param duracion tipo int
+	 * @param anio tipo int
+	 */
+	public Pelicula(String nombre, int duracion, int anio) {
+		
+	}
+
+	@Override
+	public String toString() {
+		return null;
+	}
+	/**
+	 * 
+	 * @param otraPelicula
+	 * @return el metodo es igual retorna falso
+	 */
+	public boolean esIgual(Pelicula otraPelicula) {
+		return false;
+	}
+
+	public String getNombre() {
+		return "";
+	}
+
+	public void setNombre(String nombre) {
+		
+	}
+	
+	public int getDuracion() {
+		return -1;
+	}
+
+	public void setDuracion(int duracion) {
+		
+	}
+
+	public int getAnio() {
+		return -1;
+	}
+
+	public void setAnio(int anio) {
+		
+	}
+	
+}
